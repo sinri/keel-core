49a0bd77c7f89ce3d0191a92d49971edc7a88e34
sinri <e.joshua.s.e@gmail.com>
2025-05-07 13:25:30 +0800

    Refactor ValueBox class for improved thread safety and clarity. Update documentation for methods to enhance readability and ensure consistent formatting. Synchronize access to critical methods to prevent race conditions.

--- src/main/java/io/github/sinri/keel/core/ValueBox.java
+++ src/main/java/io/github/sinri/keel/core/ValueBox.java
@@ -3,24 +3,25 @@ package io.github.sinri.keel.core;
 import javax.annotation.Nullable;
 
 /**
- * A generic container for a value that can be set with an optional expiration time.
+ * A generic container for a value that can be set with an optional expiration
+ * time.
  * <p>
  * The class allows setting and getting the value, checking if the value is set,
- * and clearing the value. If an expiration time is provided, the value will
- * be automatically cleared after the specified duration.
+ * and clearing the value. If an expiration time is provided, the value will be
+ * treated as expired, and will be cleared when been checked.
  *
  * @param <T> the type of the value to be stored in the box
  * @since 3.0.19
  */
 public class ValueBox<T> {
-    private T value;
-    private boolean valueAlreadySet;
+    private volatile T value;
+    private volatile boolean valueAlreadySet;
     /**
      * @since 3.1.0
-     *         When expire is equal or less than zero, never expire;
-     *         Or as the milliseconds to reserve the value.
+     *        When expire is equal or less than zero, never expire;
+     *        Or as the milliseconds to reserve the value.
      */
-    private long expire = 0;
+    private volatile long expire = 0;
 
     /**
      * Constructs a new instance of ValueBox and clears its internal state.
@@ -33,7 +34,8 @@ public class ValueBox<T> {
     }
 
     /**
-     * Constructs a new instance of ValueBox with the specified value and no expiration time.
+     * Constructs a new instance of ValueBox with the specified value and no
+     * expiration time.
      *
      * @param value the initial value to be stored in the box, can be null
      */
@@ -42,17 +44,19 @@ public class ValueBox<T> {
     }
 
     /**
-     * Constructs a new instance of ValueBox with the specified value and an optional expiration time.
+     * Constructs a new instance of ValueBox with the specified value and an
+     * optional expiration time.
      *
      * @param value    the initial value to be stored in the box, can be null
-     * @param lifetime the duration in milliseconds after which the value will expire and be cleared;
+     * @param lifetime the duration in milliseconds after which the value will
+     *                 expire and be cleared;
      *                 if 0 or negative, the value will not expire
      */
     public ValueBox(@Nullable T value, long lifetime) {
         this.setValue(value, lifetime);
     }
 
-    public ValueBox<T> clear() {
+    public synchronized ValueBox<T> clear() {
         this.value = null;
         this.valueAlreadySet = false;
         this.expire = 0;
@@ -63,8 +67,9 @@ public class ValueBox<T> {
      * If the value is already set, and, not expired if lifetime declared.
      * When checked, if the value set but expired, it would be cleaned.
      */
-    public boolean isValueAlreadySet() {
-        if (!valueAlreadySet) return false;
+    public synchronized boolean isValueAlreadySet() {
+        if (!valueAlreadySet)
+            return false;
         if (expire <= 0) {
             return true;
         }
@@ -76,15 +81,19 @@ public class ValueBox<T> {
     }
 
     /**
-     * Retrieves the value stored in the ValueBox if it has been set and not expired.
+     * Retrieves the value stored in the ValueBox if it has been set and not
+     * expired.
      *
-     * @return the value of type T if it is already set and not expired, otherwise throws an
+     * @return the value of type T if it is already set and not expired, otherwise
+     *         throws an
      *         {@link IllegalStateException}
      */
     @Nullable
-    public T getValue() {
-        if (isValueAlreadySet()) return value;
-        else throw new IllegalStateException("Value is not set yet");
+    public synchronized T getValue() {
+        if (isValueAlreadySet())
+            return value;
+        else
+            throw new IllegalStateException("Value is not set yet");
     }
 
     /**
@@ -93,33 +102,39 @@ public class ValueBox<T> {
      * @param value the value to be stored in the box, can be null
      * @return the current instance of ValueBox for method chaining
      */
-    public ValueBox<T> setValue(@Nullable T value) {
+    public synchronized ValueBox<T> setValue(@Nullable T value) {
         return this.setValue(value, 0);
     }
 
     /**
-     * Retrieves the value stored in the ValueBox if it has been set and not expired.
+     * Retrieves the value stored in the ValueBox if it has been set and not
+     * expired.
      * If the value is not set or has expired, returns the provided fallback value.
      *
-     * @param fallbackForInvalid the value to return if the stored value is not set or has expired
-     * @return the value of type T if it is already set and not expired, otherwise the fallback value
+     * @param fallbackForInvalid the value to return if the stored value is not set
+     *                           or has expired
+     * @return the value of type T if it is already set and not expired, otherwise
+     *         the fallback value
      * @since 3.1.0
      */
     @Nullable
-    public T getValueOrElse(@Nullable T fallbackForInvalid) {
-        if (isValueAlreadySet()) return value;
-        else return fallbackForInvalid;
+    public synchronized T getValueOrElse(@Nullable T fallbackForInvalid) {
+        if (isValueAlreadySet())
+            return value;
+        else
+            return fallbackForInvalid;
     }
 
     /**
      * Sets the value in the ValueBox with an optional expiration time.
      *
-     * @param value     the value to be stored in the box, can be null
-     * @param lifetime  the duration in milliseconds after which the value will expire and be cleared;
-     *                  if 0 or negative, the value will not expire
+     * @param value    the value to be stored in the box, can be null
+     * @param lifetime the duration in milliseconds after which the value will
+     *                 expire and be cleared;
+     *                 if 0 or negative, the value will not expire
      * @return the current instance of ValueBox for method chaining
      */
-    public ValueBox<T> setValue(@Nullable T value, long lifetime) {
+    public synchronized ValueBox<T> setValue(@Nullable T value, long lifetime) {
         this.value = value;
         this.valueAlreadySet = true;
         if (lifetime > 0) {
@@ -135,7 +150,7 @@ public class ValueBox<T> {
      *
      * @return true if the value is set and is null, false otherwise
      */
-    public boolean isValueSetToNull() {
+    public synchronized boolean isValueSetToNull() {
         return this.isValueAlreadySet() && this.getValue() == null;
     }
 }

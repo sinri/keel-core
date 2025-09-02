761479ba5cb418c31fb166f6e0b3dca699e919e4
sinri <e.joshua.s.e@gmail.com>
2025-09-02 16:47:31 +0800

    feat(cli): Introduce `KeelCliProgram` for streamlined CLI application support
    - Added an abstract `KeelCliProgram` class to simplify CLI applications.
    - Implemented error handling using `JsonifiedThrowable` with customizable exit codes.
    - Updated `pom.xml` to version 4.1.3-SNAPSHOT.
    
    Signed-off-by: sinri <e.joshua.s.e@gmail.com>

---
+++ src/main/java/io/github/sinri/keel/facade/cli/KeelCliProgram.java
@@ -0,0 +1,55 @@
+package io.github.sinri.keel.facade.cli;
+
+import io.github.sinri.keel.core.TechnicalPreview;
+import io.github.sinri.keel.core.json.JsonifiedThrowable;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * @since 4.1.3
+ */
+@TechnicalPreview(since = "4.1.3")
+public abstract class KeelCliProgram {
+
+    private KeelCliArgs cliArgs;
+
+    @Nullable
+    abstract protected KeelCliArgsParser buildCliArgParser();
+
+    public final void launch(String[] args) {
+        try {
+            var argsParser = buildCliArgParser();
+            if (argsParser != null) {
+                this.cliArgs = argsParser.parse(args);
+            } else {
+                this.cliArgs = new KeelCliArgsImpl();
+            }
+            runWithCommandLine();
+        } catch (Throwable throwable) {
+            handleError(throwable);
+        }
+    }
+
+    @Nonnull
+    public final KeelCliArgs getCliArgs() {
+        if (cliArgs == null) {
+            throw new IllegalStateException("CliArgs not initialized yet!");
+        }
+        return cliArgs;
+    }
+
+    protected void handleError(Throwable throwable) {
+        JsonifiedThrowable jsonifiedThrowable = JsonifiedThrowable.wrap(throwable);
+        System.err.println(jsonifiedThrowable.toJsonObject().encodePrettily());
+        if (throwable instanceof KeelCliArgsDefinitionError) {
+            System.exit(1);
+        } else if (throwable instanceof KeelCliArgsParseError) {
+            System.exit(2);
+        } else {
+            System.exit(3);
+        }
+    }
+
+    protected abstract void runWithCommandLine();
+}
